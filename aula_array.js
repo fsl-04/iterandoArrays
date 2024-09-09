da4885f88ea4a28ee39ecf7fa77ec3dfd9e5fa89
Carlos Vieira <112698218+carloeira@users.noreply.github.com>
2024-09-09 20:35:06 -0300

    resolução

--- aula_array.js
+++ aula_array.js
@@ -8,4 +8,34 @@ const empresas = [
     { nome: 'Apple', valorDeMercado: 845, CEO: 'Tim Cook', anoDeCriacao: 1976 }
 ];
 
-// 
+// 1. Listar o nome de todas as empresas
+const nomesMapeados = empresas.map(empresa => empresa.nome);
+console.log("Nomes das empresas: ");
+console.log(nomesMapeados);
+
+// 2. Filtrar empresas com valor de mercado acima de 100
+const valoresMapeados = empresas.filter(empresa => empresa.valorDeMercado > 100);
+console.log("Empresas com valor de mercado acima de 100: ");
+console.log(valoresMapeados);
+
+// 3. Calcular o valor total de mercado das empresas
+const valorTotalDeMercado = empresas.reduce((total, empresa) => total + empresa.valorDeMercado, 0);
+console.log(`\nValor total de mercado das empresas: ${valorTotalDeMercado}`);
+
+// 4. Encontrar a empresa mais antiga
+const empresaMaisAntiga = empresas.reduce((maisAntiga, empresa) => {
+    return (empresa.anoDeCriacao < maisAntiga.anoDeCriacao) ? empresa : maisAntiga;
+}, empresas[0]);
+console.log("\nEmpresa mais antiga: ");
+console.log(empresaMaisAntiga);
+
+// 5. Criar um novo array com nome e CEO das empresas
+const nomesECeosDasEmpresas = [];
+empresas.forEach(empresa => {
+    nomesECeosDasEmpresas.push({
+        nome: empresa.nome,
+        CEO: empresa.CEO
+    })
+});
+console.log("\nNome e CEO das empresas: ");
+console.log(nomesECeosDasEmpresas);
